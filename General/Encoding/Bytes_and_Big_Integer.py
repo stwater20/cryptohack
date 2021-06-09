ff521be66d84dedabf563fe99607a121d4ba27eb
stwater20 <54sakuramiku@gmail.com>
2021-06-09 21:37:29 +0800

    crypto

---
+++ General/Encoding/Bytes_and_Big_Integer.py
@@ -0,0 +1,5 @@
+from Crypto.Util.number import bytes_to_long,long_to_bytes
+
+string = 11515195063862318899931685488813747395775516287289682636499965282714637259206269
+
+print(long_to_bytes(string))
